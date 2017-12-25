df75c87a2f3ac14b2e2324ea933a6c53d36a7243
Laurigit <irualgm@gmail.com>
2017-12-25 19:32:11 +0200

    rukattu tiedsotojen lähetystä. Vielä pitäis tarksitaa, että testi, dev ja tuotanto menee oikein

--- code/global.R
+++ code/global.R
@@ -1,5 +1,5 @@
 #options are prod, test, dev
-GLOBAL_test_mode <- "test"
+GLOBAL_test_mode <- "prod"
 dir.create("./rdata/", showWarnings = FALSE)
 dir.create("./drop_download/", showWarnings = FALSE)
 dir.create("./decks_unzipped/", showWarnings = FALSE)
@@ -35,7 +35,8 @@ token <- readRDS("droptoken.rds")
 #drop_acc(dtoken = token)
 
 
-
+#download_folder <- "./drop_download/"
+#drop_box_folder <- "mstat/all_data/"
 load_data_from_DB <- function(download_folder, drop_box_folder, drop_box_unit_test_folder) {
   test_mode <- "prod"
   if (exists("GLOBAL_test_mode")) {
@@ -47,46 +48,25 @@ load_data_from_DB <- function(download_folder, drop_box_folder, drop_box_unit_te
   }
   
   if (test_mode == "prod") {
+    #delete prev decks in case of testing
+    do.call(file.remove, list(list.files("./decks_unzipped", full.names = TRUE)))
+    do.call(file.remove, list(list.files("./drop_download", full.names = TRUE)))
+    do.call(file.remove, list(list.files("./drop_upload", full.names = TRUE)))
+    do.call(file.remove, list(list.files("./rdata", full.names = TRUE)))
     
-  
-  tulos <- as.data.table(drop_read_csv(paste0(drop_box_folder, "divari.csv"),
-                                       dest = download_folder,
-                                       sep = ";",
-                                       stringsAsFactors = FALSE,
-                                       dtoken = token))
-  tulos <- as.data.table(drop_read_csv(paste0(drop_box_folder, "pelit.csv"),
-                                       dest = download_folder,
-                                       sep = ";",stringsAsFactors = FALSE,
-                                       dtoken = token))
-  tulos <- as.data.table(drop_read_csv(paste0(drop_box_folder, "temp_data_storage.csv"),
-                                       dest = download_folder,
-                                       sep = ";",
-                                       stringsAsFactors = FALSE,
-                                       dtoken = token))
-  tulos <- as.data.table(drop_read_csv(paste0(drop_box_folder, "turnaussaanto.csv"), 
-                                       dest = download_folder,
-                                       sep = ";",
-                                       stringsAsFactors = FALSE,
-                                       dtoken = token))
-  drop_download(path = paste0(drop_box_folder,"json.zip"),
-                local_path = download_folder,
-                overwrite = TRUE,
-                dtoken = token)
-  #delete prev decks in case of testing
-  do.call(file.remove, list(list.files("./decks_unzipped", full.names = TRUE)))
-  
+    
+    drop_download(path = paste0(drop_box_folder,"all_files.zip"),
+                  local_path = download_folder,
+                  overwrite = TRUE,
+                  dtoken = token)
+    unzip(zipfile = paste0(download_folder, "all_files.zip"),
+          exdir = "./drop_download")
+    
+    
+
   unzip(zipfile = paste0(download_folder, "json.zip"),
         exdir = "./decks_unzipped")
   
-  #tilastoasetukset
-  drop_download(path = paste0(drop_box_folder,"tilastoAsetukset.R"),
-                local = download_folder,
-                overwrite = TRUE,
-                dtoken = token)
-  drop_download(paste0(drop_box_folder, "saavutusAsetukset.R"),
-                local = download_folder,
-                overwrite = TRUE,
-                dtoken = token)
   #move .R -files to /rdata
   rfilelist <- data.table(
     filename=dir(download_folder))[,
@@ -97,7 +77,8 @@ load_data_from_DB <- function(download_folder, drop_box_folder, drop_box_unit_te
   
   }
 }
-load_data_from_DB("./drop_download/")
+load_data_from_DB(download_folder = "./drop_download/", drop_box_folder = "mstat/all_data/",
+                  drop_box_unit_test_folder = "mstat/all_data_static/")
 
 
 
@@ -107,45 +88,82 @@ kircsv <- function(datataulu, tiedostonimi, upload = TRUE) {
   if(exists("GLOBAL_test_mode")) {
     if (GLOBAL_test_mode == "dev") {
       test_mode <- "dev"
-    } else if (GLOBAL_test_mode = "test") {
+    } else if (GLOBAL_test_mode == "test") {
       upload_folder <- "mstat/all_data_for_test/"
     }
   }
   
-  write.table(x=datataulu,file=tiedostonimi,sep=";",row.names = FALSE,dec=",")
+  write.table(x = datataulu,
+              file = tiedostonimi,
+              sep = ";",
+              row.names = FALSE,
+              dec = ",")
+  
+
   if (test_mode == "prod" & upload == TRUE) {
-  drop_upload(tiedostonimi, upload_folder, mode = "overwrite" ,dtoken = token)
+    #copy to upload
+    file.copy(from = tiedostonimi, to = "./drop_upload/")
+    zip_all_and_send()
   }
 }
 
+zip_all_and_send <- function() {
+#copy rdata
+  rdatafiles <- paste0("./rdata/",  dir("./rdata/"))
+  csv_files <-  data.table(filename= dir("./drop_download/"))[, file_end :=  substr(filename, nchar(filename)-3, nchar(filename))][file_end==".csv",
+                                  paste0("./drop_download/",filename)]
+  file.copy(from= c(rdatafiles, csv_files), to = "./drop_upload", overwrite = TRUE)
+    tiedostot <- as.data.table(dir(path = "./drop_upload/"))
+
+    setwd("./drop_upload")
+    zip(zipfile = "../drop_upload/all_files.zip",
+        files = tiedostot[,V1])
+    setwd("..")
+    test_mode <- "prod"
+    upload_dir <- "mstat/all_data/"
+    if (exists("GLOBAL_test_mode")) {
+      if (GLOBAL_test_mode == "dev") {
+        test_mode <- "dev"
+      } else if (GLOBAL_test_mode == "test") {
+        upload_dir <- "mstat/all_data_for_test/"
+      }
+    }
+    if (test_mode == "prod") {
+      drop_upload("./drop_upload/all_files.zip", upload_dir, mode = "overwrite", dtoken = token)
+    }
+  
+}
 
 
-luecsv<-function(tiedostonimi) {
-  tulos <-as.data.table(read.csv(tiedostonimi,sep=";",stringsAsFactors = FALSE,dec=",",fileEncoding="UTF-8-BOM"))
+luecsv <- function(tiedostonimi) {
+  tulos <- as.data.table(read.csv(tiedostonimi,
+                                  sep = ";",
+                                  stringsAsFactors = FALSE,
+                                  dec = ",",
+                                  fileEncoding = "UTF-8-BOM"))
   return(tulos)
 }
 
 
 #pakkaa jsonit ja laheta
-zipAndSend<-function(){
-
-  tiedostot<- as.data.table(dir(path = "./decks_unzipped/"))
+zipAndSend <- function(){
+  tiedostot <- as.data.table(dir(path = "./decks_unzipped/"))
   
-  tiedostot[,paate:= substr(tiedostot[,V1], nchar(tiedostot[,V1])-5+1, nchar(tiedostot[,V1]))]
-  json_files<-tiedostot[paate==".json",paste0(V1)]
-  if (length(json_files)>0){
+  tiedostot[, paate := substr(tiedostot[,V1], nchar(tiedostot[, V1]) - 5 + 1, nchar(tiedostot[, V1]))]
+  json_files <- tiedostot[paate == ".json", paste0(V1)]
+  if (length(json_files) > 0) {
     setwd("./decks_unzipped")
       zip(zipfile = "../drop_upload/json.zip",
-          files=json_files)
+          files = json_files)
     setwd("..")
-    test_mode <- FALSE
+    test_mode <- "prod"
     if(exists("GLOBAL_test_mode")) {
-      if (GLOBAL_test_mode == TRUE) {
-        test_mode <- TRUE
+      if (GLOBAL_test_mode == "dev") {
+        test_mode <- "dev"
       }
     }
-    if (test_mode == FALSE) {
-    drop_upload("./drop_upload/json.zip", "mstat/processed/", mode = "overwrite" ,dtoken = token)
+    if (test_mode == "prod" | test_mode == "test") {
+      zip_all_and_send()
     }
   }
 
