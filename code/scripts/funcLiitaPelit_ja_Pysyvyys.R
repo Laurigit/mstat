0ddbb23f2c5b8bcf9cf39787580bdd013275a469
Laurigit <irualgm@gmail.com>
2018-02-20 14:04:19 +0000

    alettu uittaan keskihinitaan voittoennsuteeseen

--- code/scripts/funcLiitaPelit_ja_Pysyvyys.R
+++ code/scripts/funcLiitaPelit_ja_Pysyvyys.R
@@ -13,16 +13,32 @@ pysyvyys_pct<-as.data.table(pfi_data)
 #joinaa pysyvyys_pct divariin
 
 pysyvyys_pct[,':=' (dt_alku=oma_timedate(pvm,kello),dt_loppu=oma_timedate(pvm_end,kello_end))]
-laurin_pakat<-pysyvyys_pct[omistaja=="L",.(Laurin_pakka_form_id=id,Laurin_pakka=pakkanumero,dt_alku,dt_loppu,pysyvyys_pct,hinta_lauri=hinta)]
-martin_pakat<-pysyvyys_pct[omistaja=="M",.(Martin_pakka_form_id=id,Martin_pakka=pakkanumero,dt_alku,dt_loppu,pysyvyys_pct,hinta_martti=hinta)]
+laurin_pakat<-pysyvyys_pct[omistaja=="L",.(Laurin_pakka_form_id = id,
+                                           Laurin_pakka = pakkanumero,
+                                           dt_alku,
+                                           dt_loppu,
+                                           pysyvyys_pct,
+                                           hinta_lauri = hinta,
+                                           laurin_kortti_lkm = kortti_lkm)]
+martin_pakat<-pysyvyys_pct[omistaja=="M",.(Martin_pakka_form_id = id,
+                                           Martin_pakka = pakkanumero
+                                           ,dt_alku,
+                                           dt_loppu,
+                                           pysyvyys_pct,
+                                           hinta_martti = hinta,
+                                           martin_kortti_lkm = kortti_lkm)]
 pelidata_dt<-pelidata_temp_all[,':=' (pelidt_alku=oma_timedate(Aloituspvm,Aloitusaika),pelitdt_loppu=oma_timedate(Lopetuspvm,Lopetusaika))]
 
+#tilapäinen pelidtalku ja loppu
+pelidata_dt[is.na(pelidt_alku), ':=' (pelidt_alku = 4000000000, pelitdt_loppu=4000000001)]
+
+
 joiniID_and_pct_lauri<-laurin_pakat[pelidata_dt,on=c("dt_alku<pelidt_alku","dt_loppu>pelitdt_loppu","Laurin_pakka==Laurin_pakka")]
-joiniID_and_pct_lauri<-joiniID_and_pct_lauri[,.(peli_ID,Laurin_pysyvyys_pct=pysyvyys_pct,Laurin_pakka_form_id,hinta_lauri)]
+joiniID_and_pct_lauri<-joiniID_and_pct_lauri[,.(peli_ID,Laurin_pysyvyys_pct=pysyvyys_pct,Laurin_pakka_form_id,hinta_lauri,laurin_kortti_lkm)]
 #joinaa viela martti
 joiniID_and_pct_martti<-martin_pakat[pelidata_dt,on=c("dt_alku<pelidt_alku","dt_loppu>pelitdt_loppu","Martin_pakka==Martin_pakka")]
 
-joiniID_and_pct_martti<-joiniID_and_pct_martti[,.(peli_ID,Martin_pysyvyys_pct=pysyvyys_pct,Martin_pakka_form_id,hinta_martti)]
+joiniID_and_pct_martti<-joiniID_and_pct_martti[,.(peli_ID,Martin_pysyvyys_pct=pysyvyys_pct,Martin_pakka_form_id,hinta_martti,martin_kortti_lkm)]
 #joinaa tulokset
 setkey(joiniID_and_pct_lauri,peli_ID)
 setkey(joiniID_and_pct_martti,peli_ID)
--- code/scripts/func_voittoennuste.R
+++ code/scripts/func_voittoennuste.R
@@ -14,15 +14,18 @@ voittoEnnuste <- function(LP, MP, peliData_ja_pfi, LMull, MMull, Aloittaja) {
   #filtteröi data
   subset_pelit <- pelidata_joined_pakkatiedot[!is.na(Voittaja) & (Laurin_pakka == LP | Martin_pakka == MP),
                            .(Laurin_pakka, Martin_pakka, Voittaja, Laurin_mulligan,
-                              Martin_mulligan, Laurin_pysyvyys_pct, Martin_pysyvyys_pct, Aloittaja)]
+                              Martin_mulligan, Laurin_pysyvyys_pct, Martin_pysyvyys_pct, Aloittaja, hinta_lauri, hinta_martti, laurin_kortti_lkm, martin_kortti_lkm)]
   #luo muuttujat. Kertoimet sen takia, että otetaan yhteiskerroin vaan VS_peliin.
   subset_pelit[, ':=' (Mull_diff =  Martin_mulligan - Laurin_mulligan,
     VS_peli_bool = ifelse(Laurin_pakka == LP & Martin_pakka == MP, 1, 0),
     L_kerroin = ifelse(Laurin_pakka == LP, Laurin_pysyvyys_pct,1),
-    M_kerroin = ifelse(Martin_pakka == MP, Martin_pysyvyys_pct, 1))]
+    M_kerroin = ifelse(Martin_pakka == MP, Martin_pysyvyys_pct, 1),
+    laurin_keskihinta = hinta_lauri / laurin_kortti_lkm,
+    martin_keskihinta = hinta_martti / martin_kortti_lkm)]
   
   
-  subset_pelit[, ':=' (weight = L_kerroin * M_kerroin)]
+  subset_pelit[, ':=' (weight = L_kerroin * M_kerroin,
+                       keskiHintaEro = log((laurin_keskihinta / martin_keskihinta), base = exp(1)))]
 
 
 #analyysisarakkeet
@@ -30,7 +33,8 @@ analyse_cols <- subset_pelit[, .(Voittaja,
                                  Mull_diff,
                                  VS_peli_bool,
                                  weight,
-                                 Aloittaja)]
+                                 Aloittaja,
+                                 keskiHintaEro)]
 
 model <- glm(data = analyse_cols, 
              formula = Voittaja ~ Aloittaja + Mull_diff  + VS_peli_bool, 
