c9c7403c6c61b4690072872c70fe283d851b81c9
Laurigit <irualgm@gmail.com>
2019-04-18 15:14:22 +0000

    korjattu bugi, sesion on session

--- scripts/tab/tab_life_counter.R
+++ scripts/tab/tab_life_counter.R
@@ -366,7 +366,7 @@ observe({
     damage_data$data <- accRows 
     input_error$error <- FALSE
     
-    if (sesion$user != "overlay") {
+    if (session$user != "overlay") {
        shinyalert(
                  title = "Difference in damage input",
                   text = "Input has been deleted",
