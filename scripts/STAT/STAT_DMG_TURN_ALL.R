3e519e1733cf7c169195fb16509362b7bc5c58aa
Laurigit <irualgm@gmail.com>
2019-04-21 06:40:32 +0000

    avattu edellinen toimiva ja lisatty carteisan

--- scripts/STAT/STAT_DMG_TURN_ALL.R
+++ scripts/STAT/STAT_DMG_TURN_ALL.R
@@ -26,7 +26,7 @@ join_seq <- seq_data[temp, on = "TSID"]
 aloittajadata <- pakat[, .(Peli_ID, Aloittaja, Pakka_ID,
                                              Vastustajan_Pakka_ID, Omistaja_ID,
                            Vastustajan_Omistaja_ID)]
-join_aloittaja <- aloittajadata[join_seq, on = "Peli_ID"]
+join_aloittaja <- aloittajadata[join_seq, on = "Peli_ID", allow.cartesian =TRUE]
 #fix reverse damage ja combat damage- yhteys
 join_aloittaja[, Combat_dmg := ifelse(Target_player == Dmg_source, 0, Combat_dmg)]
 aggregate_overinput <- join_aloittaja[, .(Amount = mean(Amount)), 
