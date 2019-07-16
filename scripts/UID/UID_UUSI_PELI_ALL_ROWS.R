1c76a2ec3110a1a207cf6df8447f021ab88892c1
Laurigit <lauri>
2019-07-16 16:44:28 +0000

    palautettu edellinen muutis

--- scripts/UID/UID_UUSI_PELI_ALL_ROWS.R
+++ scripts/UID/UID_UUSI_PELI_ALL_ROWS.R
@@ -40,7 +40,7 @@ UID_UUSI_PELI_ALL_ROWS <- function(
   
   
   required_data(c("STAT_CURRENT_PAKKA", "STG_PAKAT", "STG_OMISTAJA", "ADM_PELIT", "STAT_VOITTOENNUSTE"))
-
+  
   pelidata <- ADM_PELIT[1 != 0]
   
   
@@ -51,31 +51,21 @@ UID_UUSI_PELI_ALL_ROWS <- function(
   #Left_pakka <- pelidata[Peli_ID == Peli_ID_input & Omistaja_ID=="L", .(Pakka_ID)]
   #Right_pakka <- pelidata[Peli_ID == Peli_ID_input & Omistaja_ID=="M", .(Pakka_ID)]
   #Divari <- pelidata[Peli_ID == Peli_ID_input & Omistaja_ID=="M", .(Divari)]
- # browser
-  Pakka <- UID_PAKKA[Pakka_ID == 3 | Pakka_ID == 36]
-  PakkaVS_subset <- UID_PAKKA_VS[Pakka_ID == 3| Pakka_ID == 36]
   
-  max_turnaus <- pelidata[, max(Turnaus_NO)]
-  turnausparit <- pelidata[Turnaus_NO == max_turnaus, .(Peli_ID, Pakka_ID, Vastustajan_Pakka_ID)]
-  
-  PakkaVS <-  PakkaVS_subset[turnausparit, on = .(Pakka_ID, Vastustajan_Pakka_ID)]
-  
+  Pakka <- UID_PAKKA
+  PakkaVS <- UID_PAKKA_VS
   #message("PakkaVS ", PakkaVS)
  # Tilanne <- getTilanne(pelidata, Peli_ID_input)
  # Aloittaja <- pelidata[Peli_ID == Peli_ID_input , .(Pakka_ID, Aloittaja)]
   
   #join dybamic color
-
   sscols_pakat <- STAT_CURRENT_PAKKA[, .(Pakka_NM, Pakka_ID, Colors, Most_same_card, Most_wins_sames_card,
-                                         Pakka_NM_Dynamic, Omistaja_ID)][Pakka_ID == 3 | Pakka_ID == 36]
+                                         Pakka_NM_Dynamic, Omistaja_ID)]
   
-  joini_ilman_omistajaa <- PakkaVS[Pakka,
+  joini <- PakkaVS[Pakka,
                                    on = "Pakka_ID"][sscols_pakat,
-                                                                    on = "Pakka_ID"]
-  joini <- STG_OMISTAJA[joini_ilman_omistajaa,   on = "Omistaja_ID"]
-  
-
-                                                                                   
+                                                                    on = "Pakka_ID"][STG_OMISTAJA,
+                                                                                     on = "Omistaja_ID"]
   joini_ssrows <- joini#[Pakka_ID %in%  c(Left_pakka,
                                         # Right_pakka)]
   joini_ssrows[, ':=' (#Pakka_ID = NULL,
@@ -92,7 +82,7 @@ UID_UUSI_PELI_ALL_ROWS <- function(
   # ennuste <- predict_result(Peli_ID_input, input_left_mulligan,
   #                           input_right_mulligan,
   #                           STAT_VOITTOENNUSTE)
-  ennuste <- STAT_VOITTOENNUSTE[, .(Prediction = ennuste, Peli_ID, Prediction_mirror = 1 - ennuste, Pakka_ID)][Pakka_ID == 3]
+  ennuste <- STAT_VOITTOENNUSTE[, .(Prediction = ennuste, Peli_ID, Prediction_mirror = 1 - ennuste, Pakka_ID)]
   
   max_turnaus <- pelidata[, max(Turnaus_NO)]
   curr_turn_pelit <- pelidata[Turnaus_NO == max_turnaus , .(Peli_ID,
@@ -102,17 +92,10 @@ UID_UUSI_PELI_ALL_ROWS <- function(
                                                             Divari,
                                                             Ottelu_ID,
                                                             Voittaja,
-                                                            Aloitus_DT)][Pakka_ID == 3 | Pakka_ID == 36]
-  join_stats <- joini_ssrows[curr_turn_pelit, on = .(Pakka_ID, Vastustajan_Pakka_ID, Peli_ID)]
+                                                            Aloitus_DT)]
+  join_stats <- joini_ssrows[curr_turn_pelit, on = .(Pakka_ID, Vastustajan_Pakka_ID)]
   
-  # #join_omistaja-uusiks
-  # omistajadata <- STG_PAKAT[, .(Pakka_ID, Omistaja_ID, Pakka_NM, Most_same_card,
-  #                               Most_wins_sames_card,
-  #                               Pakka_NM_Dynamic)][STG_OMISTAJA, on = "Omistaja_ID"]
-
   join_ennuste <- ennuste[join_stats, on = c("Peli_ID", "Pakka_ID")]
- # join_ennuste[, ':=' (Omistaja_ID = NULL, Omistaja_NM = NULL)]
-  #join_omistaja <- omistajadata[join_ennuste, on = "Pakka_ID"]
   
   tulos <- join_ennuste
   return(tulos)
