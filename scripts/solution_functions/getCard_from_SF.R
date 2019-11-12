859fb41750d6958c65ae9c23ada5a0a2352c562d
Laurigit <lauri>
2019-11-12 15:39:45 +0000

    backupi, jos ei oo multiverse_IDta

--- scripts/solution_functions/getCard_from_SF.R
+++ scripts/solution_functions/getCard_from_SF.R
@@ -12,9 +12,17 @@ getCard_from_SF <- function(input_card_name) {
   raw.result <- GET(url = url)
   result_json <- fromJSON(rawToChar(raw.result$content))
   mid <- result_json$multiverse_ids
+  if (!is.numeric(mid)) {
+    #backupMID
+    #https://api.magicthegathering.io/v1/cards?name=%22Goblin%20Guide%22
+   # urli_bu <- paste0('https://api.magicthegathering.io/v1/cards?name=\"',  input_card_name, '\"')
+    urli_bu <- paste0('https://api.magicthegathering.io/v1/cards?name=\'',  urlName, '\'')
+    raw.result_bu1 <- GET(url = urli_bu)
+    result_json_bu <- fromJSON(rawToChar(raw.result_bu1$content))
+    mid <- max(result_json$cards$multiverseid, na.rm = TRUE)
+  }
 
-  
-  
+
   result_row <- data.table( 
                             Name = iconv(x = result_json$name, to = "UTF-8"), #Name = gsub("[[:punct:]]", "", result_json$name),
                             Text = iconv(x = ifelse(is.null(result_json$oracle_text), NA,  result_json$oracle_text), to = "UTF-8"),
@@ -25,7 +33,7 @@ getCard_from_SF <- function(input_card_name) {
                             Stats = paste0(ifelse(is.null(result_json$power), NA, result_json$power),
                                            "/",
                                            ifelse(is.null(result_json$toughness), NA, result_json$toughness)),
-                            MID = result_json$multiverse_ids)
+                            MID = mid)
   
   
   return(result_row)
