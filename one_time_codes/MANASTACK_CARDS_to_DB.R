c4a782e3384f635c9246518300d73d6cc2df2973
Laurigit <lauri>
2019-11-12 16:40:16 +0000

    yritetty korajata ae

--- one_time_codes/MANASTACK_CARDS_to_DB.R
+++ one_time_codes/MANASTACK_CARDS_to_DB.R
@@ -1,6 +1,6 @@
-#fixed <- MANASTACK_CARDS
-#fixed[, Text := str_replace_all(Text, "[^[:alnum:]]", " ") ]
-
+fixed <- STG_MANASTACK_CARDS
+fixed[, Text := gsub("Æ", "Ae", Text)]
+#fixed[1:20, Name]
 #Encoding(fixed[, Text]) <- "UTF-8"
 #dbSendQuery(con, 'set character set "utf8"')
-#dbWriteTable(con, "CARDS_DIM", MANASTACK_CARDS, append = TRUE, row.names = FALSE, fileEncoding = "UTF-8")
+#dbWriteTable(con, "CARDS_DIM", fixed, append = FALSE, row.names = FALSE, fileEncoding = "UTF-8", overwrite = TRUE)
