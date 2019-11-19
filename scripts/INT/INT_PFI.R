ce41369e2bd6d26a5da004c386269cb7c89cf9f3
Laurigit <lauri>
2019-11-19 09:55:05 +0000

    INT_FPI, pysyys prossa kaikilla 100. Korjaa joskus

--- scripts/INT/INT_PFI.R
+++ scripts/INT/INT_PFI.R
@@ -1,7 +1,7 @@
 required_data(c("STG_PFI", "STG_PAKKA_COMPONENTS"))
 #calc pysyvyys
 temp <- STG_PFI
-temp[,Pakka_form_pct := (calc_pysyvyys_pct(Pakka_form_ID, Current_Pakka_form_ID, STG_PAKKA_COMPONENTS)),
+#temp[,Pakka_form_pct := (calc_pysyvyys_pct(Pakka_form_ID, Current_Pakka_form_ID, STG_PAKKA_COMPONENTS)),
      by = Pakka_form_ID]
-#temp[, Pakka_form_pct := 1]
+temp[, Pakka_form_pct := 1]
 INT_PFI <- temp
