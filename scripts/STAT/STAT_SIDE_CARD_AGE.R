9b1e157a3654c11992d95156c07cb0515f08c6d1
Laurigit <lauri>
2019-12-19 14:16:38 +0000

    tyhm typo

--- scripts/STAT/STAT_SIDE_CARD_AGE.R
+++ scripts/STAT/STAT_SIDE_CARD_AGE.R
@@ -99,7 +99,7 @@ STAT_SIDE_CARD_AGE <- join_omi[,. (Pakka_ID,
                                    Card_age = max_turnaus - Turnaus_NO)]
 
 con <- connDB(con)
-dbWT(con, STAT_SIDE_CARD_aAGE)
+dbWT(con, STAT_SIDE_CARD_AGE)
 # sidet <- STAT_SIDE_CARD_AGE[ Side == 1][, .N, by = .(Omistaja_ID, Card_age)][order(Card_age)]
 # side_agg <- STAT_SIDE_CARD_AGE[Side == 1, sum(Count), by = .(Card_age, Omistaja_ID)][order(Card_age)]
 # 
