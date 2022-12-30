d94d15567a469b94a07150feb13accb1ea35357b
Laurigit <lauri>
2022-12-30 05:49:27 +0000

    added token search

---
+++ dev_tools/tokens.R
@@ -0,0 +1,74 @@
+STAT_CURRENT_PAKKA_COMPONENTS
+active <- ADM_DIVARI[, Pakka_ID]
+cp <- copy(STAT_CURRENT_PAKKA_COMPONENTS)[Pakka_ID %in% active]
+cp[, .N, by = Pakka_ID]
+cp[, row_id := seq_len(.N)]
+find_token <- cp[, res := grep("token", Text), by = row_id]
+
+find_token[, subis := (str_extract_all(Text, "Create(.*?)token"))]
+find_token[, subis]
+
+regelist <- c("(?<=Create).*(?=token)", "(?<=create).*(?=token)" ,"(?<=Put).*(?=token)" ,"(?<=put).*(?=token)")
+totres <- NULL
+tot_dt <- NULL
+for (regeloop in regelist) {
+find_token[, subis2 := (str_extract_all(Text, regeloop))]
+#find_token[, subis3 := (str_extract_all(Text, "(?<=create).*(?=token)"))]
+#find_token[, subis4 := (str_extract_all(Text, "(?<=put).*(?=token)"))]
+#find_token[, subis5 := (str_extract_all(Text, "(?<=Put).*(?=token)"))]
+#find_token[, all_list := list(list(subis2[[1]], subis3[[1]], subis4[[1]], subis5[[1]]))]
+find_token[, row_id := seq_len(.N)]
+#find_token[, leng := nchar(subis2[[1]]), by=row_id]
+find_token[1, all_list]
+found <- find_token#[leng>0]
+
+#found[,  grep(pattern, subis2 , value = TRUE)]
+#matches <- unlist(str_extract_all(found[, subis2], "\\b[A-Z]\\w+"))
+
+found[, toksut := (str_extract_all(found[, subis2], "\\b[A-Z]\\w+"))]
+ss_found <- found[, .(Pakka_NM, toksut, Omistaja_ID)]
+dt_unlisted <- ss_found[,.(toksut = unlist(toksut)), by = setdiff(names(ss_found), 'toksut')]
+
+tot_dt <- rbind(dt_unlisted, tot_dt)
+
+
+#aggre2 <- found[, toksulist3 = list(toksut),), by = Pakka_ID]
+aggre3 <- found[, .(toksulist = (list(toksut)))]
+#aggre4<- found[, .(toksulist = (list(all_list)))]
+printtaa <- aggre3[, unique(unlist(toksulist))]
+totres <- c(totres, printtaa)
+}
+tot_dt_agg <- tot_dt[, .N, by = .(Pakka_NM, toksut, Omistaja_ID)]
+analyss <- tot_dt_agg[, .(pakat = paste0(Pakka_NM, collapse = " "), count_pakat = .N, omistajat = paste0(unique(sort(Omistaja_ID)), collapse = "")), by = toksut]
+
+analyss[order(pakat)]
+uniikit <- unique(totres)
+
+
+#aggre <- found[, .(toksut = paste0( (str_extract_all(found[, subis2], "\\b[A-Z]\\w+")), collapse = " "))]
+#aggre[2]
+
+
+find_token[, subis2]
+find_token[, res2 := str_replace_all(res, "^.*?\\.(?=\\s|$)", "")]
+tokentext <- find_token[!is.na(res2), str_sub(Text, 2, -1)]
+tokentext[1]
+names <- data.table(name = unlist(str_extract_all(tokentext, "\\b[A-Z]\\w+")))
+coutnti <- names[, .N, by = name][order(-N)]
+coutnti[1:100]
+
+STG_DECKS_DIM
+
+text <- "This is a sentence. These are some words after a dot."
+
+# Use str_replace_all to replace all matches with an empty string
+result <- str_replace_all(text, "^.*?\\.(?=\\s|$)", "")
+
+# Print the result
+print(result)
+
+
+input_string <- "This is a Test string with Some capitalized Words."
+pattern <- "\\b[A-Z][a-z]*\\b"
+
+matches <- grep(pattern, input_string, value = TRUE)
