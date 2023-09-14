491f6d00f7a8bf5ea0bfe377b0f3bb11171335bb
ll <lauri>
2023-09-14 08:46:56 +0000

    removed timestamp from damage source files

--- scripts/SRC/SRC_DMG_TURN_ALL.R
+++ scripts/SRC/SRC_DMG_TURN_ALL.R
@@ -4,6 +4,10 @@ files <- data.table(file = dir("./external_files/"))
 dmg_files <- files[substr(file, 1, 3) == "dmg"]
 total <- NULL
 for(looppi in 1:nrow(dmg_files)) {
-  total <- rbind(total, luecsv(dmg_files[looppi, file]))
+  
+  loop_data <- luecsv(dmg_files[looppi, file])
+  #lisätty dmg_ttimestamp proxyprelluja varten. csv-filuit ennen sitä ei sisällä tuota timestamp sarkaetta. Sen jälkeen sisältää. Nullataan se pois.
+  loop_data[, dmg_timestamp := NULL]
+  total <- rbind(total, loop_data)
 }
 SRC_DMG_TURN_ALL <- total
